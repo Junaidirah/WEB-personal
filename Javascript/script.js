dfde028c825b1a32b6b995fd72962c609f04be4b
Junaidi Rahmat <junaidirahmat02@gmail.com>
2023-11-13 19:47:16 +0700

    adding some js

---
+++ Javascript/script.js
@@ -0,0 +1,11 @@
+new TypeIt("#simpleUsage", {
+  strings: ".",
+  speed: 50,
+  waitUntilVisible: true,
+}).go();
+
+new TypeIt(".type", {
+  strings: "",
+  speed: 50,
+  waitUntilVisible: true,
+}).go();
